c34b296ee2f4bbe61b481e111af73dee12c671ae
doogd <misterthreed@live.fr>
2012-09-28 13:41:13 +0200

    [Dev] update all

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+(function(){
+    $(document).ready(function(){
+
+        $("aside ul li a").click(function(){
+            var anchor = $(this).attr('href');
+
+        });
+/*
+        $("#right").click(function(){su
+            $(".carre").animate({"left": "+=50px"}, "slow");
+        });
+        */
+    });
+})();

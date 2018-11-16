c5e5e8e9a0d657001d1fc2c7d119f7da119b8e3e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-16 01:18:30 +0000

    Deploy douglasduteil/douglasduteil.github.io to github.com/douglasduteil/douglasduteil.github.io.git:master

---
+++ achievements.js
@@ -0,0 +1,51 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{79:function(t,e,i){"use strict";var a=i(7),n=i(80),o=i.n(n);const l=a.a.createStyleSheet(o.a);l.addRules({section:{backgroundColor:t=>t.bgColor},title:{backgroundColor:t=>t.bgColor}});const{classes:r}=l,p={};e.a=(t=>({title:e,children:i,bgColor:a,watermark:n})=>(l.update({bgColor:a}),t(p,":page-layout")`
+    <div class=${r.watermark}>${n}</div>
+    <h1 class=${r.title}>${e}</h1>
+    <section class=${r.section}>${i}</section>
+    <style>${{html:l.toString()}}</style>
+  `))},80:function(t,e){t.exports={"@global":{"@media (min-width: 768px)":{main:{paddingLeft:"70px"}}},title:{position:"fixed",top:"0",left:"0",right:"0",zIndex:"100",transform:"translateZ(0)",willChange:"transform",textAlign:"center",margin:"0",padding:"1rem 0","@media (min-width: 768px)":{left:"70px"}},section:{flex:"1",display:"flex",padding:"20px",marginBottom:"50px","@media (min-width: 768px)":{marginBottom:"0",paddingBottom:"0"}},watermark:{position:"fixed",top:"0",bottom:"0",left:"0",right:"0",zIndex:"0",opacity:"0.1",display:"flex",alignItems:"center",justifyContent:"center","@global svg":{width:"170px",height:"170px"}}}},91:function(t,e){t.exports={"@global":{section:{"@media (max-width: 767px)":{marginBottom:"50px - 20px"},"@media (min-width: 768px)":{paddingRight:"15px * 2",justifyContent:"center"}}},ul:{padding:"0",listStyle:"none",position:"relative",display:"table",width:"100%",zIndex:"0",color:"black",margin:"0 auto 30px","&:after":{content:"''",display:"block",position:"absolute",zIndex:"-1",left:"33px",top:"0",bottom:"-30px",width:"10px",marginLeft:"-5px",backgroundColor:"rgba(0, 0, 0, 0.3)"}},"event-list":{listStyle:"none",padding:"0",margin:"0"},event:{position:"relative","@media (min-width: 992px)":{margin:"auto",width:"768px"}},icon:{width:"40px",height:"40px",speak:"none",position:"absolute",color:"#fff",borderRadius:"50%",backgroundColor:"#4f8c86",padding:"4px",boxShadow:"0 0 0 4px #fff",left:"34px",top:"-4px",margin:"0 0 0 -25px",display:"flex",alignItems:"center",justifyContent:"center","@global svg":{width:"24px",height:"24px"},"@media (max-width: 767px)":{fontSize:"1.4em"}},body:{position:"relative",margin:"5px 0 30px 93px",backgroundColor:"white",padding:"1rem 2rem","@global a":{color:"#333"},"> h3":{margin:"0 0 5px"},"&:after":{content:"' '",right:"100%",height:"0",width:"0",position:"absolute",border:"solid transparent",pointerEvents:"none",borderRightColor:"#fff",borderWidth:"20px",top:"0"}},year:{display:"block",margin:"113.33333px 0 1em",paddingLeft:"48px",fontSize:"50px",fontWeight:"bold",color:"rgba(0, 0, 0, 0.6)"}}},92:function(t){t.exports=[["2018",[{type:"talk",where:"Talk at the Bouffe Front",title:"How to build a cow ?",link:{href:"https://twitter.com/olegskl/status/1045011688557355009",name:"Bouffe Front @SFEIR"}}]],["2014",[{type:"talk",where:"Talk at the ngParis meetup",title:"Waaa Again !?",link:{href:"https://www.meetup.com/AngularJS-Paris/events/192808622/",name:"Meetup AngularJS #13 @SFEIR"}},{type:"talk",where:"Talk at the ngParis meetup",title:"Input[Range] over 9000",link:{href:"https://www.meetup.com/AngularJS-Paris/events/185603592/",name:"Meetup AngularJS #12 @BeMyAppFr"}},{type:"talk",where:"Talk at the ngParis meetup",title:"We need to make UI.Utils util again !",link:{href:"https://www.meetup.com/AngularJS-Paris/events/165733502/",name:"Meetup AngularJS #8 @SFEIR"}}]],["2013",[{type:"talk",where:"Talk at the ngParis meetup",title:"WaaaAnimation",link:{href:"https://www.meetup.com/AngularJS-Paris/events/147070122/",name:"Meetup #5 @Deezer"}},{type:"job",body:"Software development company",title:"Join SFEIR Paris"},{type:"study",body:"MSc Human-Compute",title:"Graduated from Paris 8"},{type:"github",body:"A open source organisation that buld widgets and directives for AngularJS",title:"Join Angular UI"}]]]},98:function(t,e,i){"use strict";i.r(e),i.d(e,"backgoundColor",function(){return m});var a=i(7),n=i(79),o=i(6),l=i(91),r=i.n(l),p=i(92);const s=a.a.createStyleSheet(r.a),{classes:d}=s,c={},m="#4F8C86";e.default=(({hyper:{wire:t}},e,i)=>Object(n.a)(t)({title:"Achievements",bgColor:m,watermark:Object(o.h)(t,c),children:t(c,":achivement")`
+    <article>${function(t){return e=>t()`
+    <ul class=${d.ul}>
+    ${e.map(([e,i])=>t()`
+        <li>
+          ${function(t){return e=>t()`<time class=${d.year} datetime="${new Date(e)}">${e}</time>`}(t)(e)}
+
+          <ul class=${d["event-list"]}>
+          ${i.map(e=>t()`
+              <li class=${d.event}>${g[e.type](t)(e)}</li>
+            `)}
+          </ul>
+        </li>
+      `)}
+    </ul>`}(t)(p)}</article>
+
+    <style>${{html:s.toString()}}</style>
+  `}));const g={job:t=>e=>t()`
+    <i class=${d.icon}>${Object(o.g)(t,e)}</i>
+    <div class=${d.body}>
+
+      <h3>${e.title}</h3>
+      <p>${e.body}</p>
+    </div>
+    `,study:t=>e=>t()`
+    <i class=${d.icon}>${Object(o.d)(t,e)}</i>
+    <div class=${d.body}>
+
+      <h3>${e.title}</h3>
+      <p>${e.body}</p>
+    </div>
+    `,github:t=>e=>t()`
+    <i class=${d.icon}>${Object(o.c)(t,e)}</i>
+    <div class=${d.body}>
+
+      <h3>${e.title}</h3>
+      <p>${e.body}</p>
+    </div>
+    `,talk:t=>e=>t()`
+    <i class=${d.icon}>${Object(o.i)(t,e)}</i>
+    <div class=${d.body}>
+      <h3>${e.where}</h3>
+      <p><em>"${e.title}"</em></p>
+      <p><a href="${e.link.href}">${e.link.name}</a></p>
+    </div>
+      `}}}]);
---
+++ precache-manifest.c65bde847f751b3452a2f5a5c2e6f6e6.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "165014d0267a83a3248355654c3ace8c",
+    "url": "icons/favicon-32x32.png"
+  },
+  {
+    "revision": "759e5579fd957cb2a3ac",
+    "url": "main.js"
+  },
+  {
+    "revision": "cbc339e79164abcf1d68",
+    "url": "me.js"
+  },
+  {
+    "revision": "73dc4dcc120ae6c3ddbe02dd278b08c2",
+    "url": "rockets.html"
+  },
+  {
+    "revision": "947134b373242d5a5631",
+    "url": "rockets.js"
+  },
+  {
+    "revision": "aeb5417a04fe25316b26295dab73b3aa",
+    "url": "me.html"
+  },
+  {
+    "revision": "7a1ca3298b5a0b316848",
+    "url": "vendors~stalker.js"
+  },
+  {
+    "revision": "3867c3bef66a2eac6155",
+    "url": "stalker.js"
+  },
+  {
+    "revision": "d0c71f4a220b549e55cd8cddb5c23a92",
+    "url": "icons/icon-152x152.png"
+  },
+  {
+    "revision": "bfdb900142801af384836d64ec3e1192",
+    "url": "icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "41eeb7151f72ef76278c28c4459ea4bf",
+    "url": "icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "4645fea58f6d6b2fb2abafc269537009",
+    "url": "icons/apple-touch-icon-57x57.png"
+  },
+  {
+    "revision": "e4c0d86beae37c30cf7245120d3be3e5",
+    "url": "icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "e49d2460a294d3fc701d8f82f04c29e9",
+    "url": "icons/apple-touch-icon-72x72.png"
+  },
+  {
+    "revision": "9e392a62e138e803efa0663828f92056",
+    "url": "icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "4ca576c3f1ebedc18c160c81d6ed6073",
+    "url": "icons/apple-touch-icon-precomposed.png"
+  },
+  {
+    "revision": "41eeb7151f72ef76278c28c4459ea4bf",
+    "url": "icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "db3a3d8da415cd6b63519f60386292be",
+    "url": "icons/browserconfig.xml"
+  },
+  {
+    "revision": "7b489aa708f9f297493945a1f40bec42",
+    "url": "icons/favicon-160x160.png"
+  },
+  {
+    "revision": "ff92810eed5a5da34ec30344c72901e4",
+    "url": "icons/favicon-16x16.png"
+  },
+  {
+    "revision": "5a9674b958aa58671d4024256118fb5c",
+    "url": "icons/favicon-196x196.png"
+  },
+  {
+    "revision": "aeb5417a04fe25316b26295dab73b3aa",
+    "url": "index.html"
+  },
+  {
+    "revision": "3ade000373d875e48ca9a59ed40efc38",
+    "url": "icons/favicon-96x96.png"
+  },
+  {
+    "revision": "d8fa058096d7f5d4a245054ddb615e96",
+    "url": "icons/icon-128x128.png"
+  },
+  {
+    "revision": "772c70912f02d5b5a206cf0c07a89998",
+    "url": "icons/icon-144x144.png"
+  },
+  {
+    "revision": "38d3098b8ebf5be088c1d2b147197a22",
+    "url": "icons/apple-touch-icon-144x144.png"
+  },
+  {
+    "revision": "496343a27c84b86a3340c7abf8b6e69e",
+    "url": "icons/icon-192x192.png"
+  },
+  {
+    "revision": "3c38e4aacdf5463505fc493b62539e4d",
+    "url": "icons/icon-384x384.png"
+  },
+  {
+    "revision": "bbdd5eba45e966499ad88ba2c9908429",
+    "url": "icons/icon-512x512.png"
+  },
+  {
+    "revision": "0af6fe5d37bd3e263e275399503b6f03",
+    "url": "icons/icon-72x72.png"
+  },
+  {
+    "revision": "f3cbef970c15279adc8a813fb3888372",
+    "url": "icons/icon-96x96.png"
+  },
+  {
+    "revision": "0fafa2c56a454bae274ca8ddf4579c30",
+    "url": "icons/mstile-144x144.png"
+  },
+  {
+    "revision": "e6bc948fb93140aa1ff068c11ef7a428",
+    "url": "icons/mstile-150x150.png"
+  },
+  {
+    "revision": "26c87e16b9360d6d9be27330db6a243c",
+    "url": "icons/mstile-310x150.png"
+  },
+  {
+    "revision": "e93024f72e03fc3bc414e9c003d9ea07",
+    "url": "icons/mstile-310x310.png"
+  },
+  {
+    "revision": "81f5712b6d6c1295a7104945cbd6e2e2",
+    "url": "icons/mstile-70x70.png"
+  },
+  {
+    "revision": "e158b17383f176dd370e09431676b4c0",
+    "url": "icons/favicon.ico"
+  },
+  {
+    "revision": "1ab95bd500422863e46a8a2c803718c5",
+    "url": "icons/apple-touch-icon-114x114.png"
+  },
+  {
+    "revision": "a3265e339f1d00be7a13",
+    "url": "contact.js"
+  },
+  {
+    "revision": "9323659515c812a57f7ffe21cd0c919d",
+    "url": "contact.html"
+  },
+  {
+    "revision": "22dd1f594f2cf228f509",
+    "url": "achievements.js"
+  },
+  {
+    "revision": "7877df9381c578c3b103f34b1b0a8408",
+    "url": "achievements.html"
+  }
+];

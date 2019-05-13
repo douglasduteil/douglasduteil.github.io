e1ce27dc8b6db25b52a8e39d587ccda4f3413855
Douglas Duteil <douglasduteil@gmail.com>
2019-05-13 15:33:14 +0200

    feat(contact): add rel=noreferrer to external links
    \from lighthouse
    
    *Links to cross-origin destinations are unsafe*
    Add `rel="noopener"` or `rel="noreferrer"` to any external links to improve performance and prevent security vulnerabilities.
    
    See https://developers.google.com/web/tools/lighthouse/audits/noopener

--- src/app/contact.js
+++ src/app/contact.js
@@ -22,32 +22,55 @@ export default ({ hyper: { wire } }, state, emit) =>
     children: wire(_, ':contact')`
   <ul class=${classes.ul}>
     <li class=${classes.li}>
-    <a target="_blank"
-    class=${classes.a}  href="mailto:douglasduteil@gmail.com">Gmail
-
-    </a>
-  </li>
+      <a
+        class=${classes.a}
+        href="mailto:douglasduteil@gmail.com"
+        rel="noreferrer"
+        target="_blank"
+      >
+          Gmail
+      </a>
+    </li>
     <li class=${classes.li}>
-    <a target="_blank"
-    class=${classes.a}  href="https://google.com/+DouglasDuteil">Google +
-  </a>
-  </li>
+      <a
+        class=${classes.a}
+        href="https://google.com/+DouglasDuteil"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Google +
+      </a>
+    </li>
     <li class=${classes.li}>
-    <a target="_blank"
-    class=${classes.a}  href="https://twitter.com/douglasduteil">Twitter
-  </a>
-  </li>
+      <a
+        class=${classes.a}
+        href="https://twitter.com/douglasduteil"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Twitter
+      </a>
+    </li>
     <li class=${classes.li}>
-    <a target="_blank"
-    class=${classes.a}  href="https://www.facebook.com/douglas.duteil">Facebook
-  </a>
-  </li>
+      <a
+        class=${classes.a}
+        href="https://www.facebook.com/douglas.duteil"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Facebook
+      </a>
+    </li>
     <li class=${classes.li}>
-    <a target="_blank"
-    class=${classes.a}  href="mailto:douglasduteil@outlook.com">Outlook
-
-    </a>
-  </li>
+      <a
+        class=${classes.a}
+        href="mailto:douglasduteil@outlook.com"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Outlook
+        </a>
+    </li>
   </ul>
   <style>${{ html: sheet.toString() }}</style>
   `
